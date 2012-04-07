5c89b9f7814fc3cdf76e5ced07983a7b8e51bf6c
Steven Arnow <steven@slaeshjag.org>
2012-04-07 16:29:49 +0200

    Addec the bbox-code from master, will finish it here..

---
+++ src/bbox.c
@@ -0,0 +1,102 @@
+#include "darner.h"
+
+
+int bboxAdd(void *handle, unsigned int x, unsigned int y, unsigned int w, unsigned int h) {
+	DARNER *m = handle;
+	int i;
+
+	for (i = 0; i < BBOX_MAX; i++)
+		if (m->bbox.bbox[i].key == -1) {
+			m->bbox.bbox[i].x = x, m->bbox.bbox[i].y = y, m->bbox.bbox[i].w = w;
+			m->bbox.bbox[i].h = h, m->bbox.bbox[i].xb = x+w, m->bbox.bbox[i].yb = y+h;
+			m->bbox.bbox[i].key = m->bbox.cnt;
+			m->bbox.cnt++;
+			m->bbox.bbox[i].bboxes++;
+			return m->bbox.bbox[i].key;
+		}
+	
+	return -1;
+}
+
+
+void bboxClear(void *handle) {
+	DARNER *m = handle;
+	int i;
+
+	for (i = 0; i < BBOX_MAX; i++)
+		m->bbox.bbox[i].key = -1;
+	
+	m->bbox.bboxes = 0;
+	m->bbox.cnt = 0;
+	m->bbox.sort = 0;
+
+	return;
+}
+
+
+int bboxInit(void *handle) {
+	DARNER *m = handle;
+
+	bboxClear();
+	m->bbox.sortmode = SORT_MODE_X;
+	
+	return 0;
+}
+
+
+
+int bboxCollBoxTest(void *handle, unsigned int x, unsigned int y, unsigned int w, unsigned int h, unsigned int *list, unsigned int listlen) {
+	DARNER *m = handle;
+	DARNER_BBOX_ENTRY tmp;
+	int i, test, no;
+
+	if (m->bbox.sort)
+		bboxSort(m);
+	
+	test = i = m->bbox.bboxes>>1;
+	i >>= 1;
+
+	if (m->bbox.sortmode == SORT_MODE_X) {
+		for (; i > TARGET_FILTER; i>>=1) {
+			if (m->bbox.bbox[test+i].xb < x)
+				test += i;
+			else if (m->bbox.bbox[test].xb < x);
+			else
+				test -= i;
+		}
+
+		if (m->bbox.bbox[test].xb > x)
+			test = 0;
+	} else {
+		for (; i > TARGET_FILTER; i>>=1) {
+			if (m->bbox.bbox[test+i].yb < y)
+				test += i;
+			else if (m->bbox.bbox[test].yb < y);
+			else
+				test -= i;
+		}
+
+		if (m->bbox.bbox[test].yb > y)
+			test = 0;
+	}
+	
+	no = 0;
+
+	for (i = test; i < m->bbox.bboxes; i++) {
+		if (m->bbox.sort == SORT_MODE_X && m->bbox.bbox[i].x > x+w)
+			break;
+		if (m->bbox.sort == SORT_MODE_Y && m->bbox.bbox[i].y > y+h)
+			break;
+		if (m->bbox.bbox[i].xb <= x || m->bbox.bbox[i].x >= x+w)
+			continue;
+		if (m->bbox.bbox[i].yb <= y || m->bbox.bbox[i].y >= y+h)
+			continue;
+		if (no == listlen)
+			break;
+		list[no] = m->bbox.bbox[i].key;
+		no++;
+	}
+
+	return no;
+}
+
