2bcb116bc1c6524659db00c5ac9c80342d3c8a5b
Steven Arnow <steven@slaeshjag.org>
2012-07-06 14:19:05 +0200

    Fixed function names in UTF-8 api

--- src/api/darnit_utf8.c
+++ src/api/darnit_utf8.c
@@ -1,36 +1,36 @@
 #include "darnit.h"
 
 
-int EXPORT_THIS utf8CharLenghtGet(const char *str) {
+int EXPORT_THIS daenitUtf8CharLenghtGet(const char *str) {
 	return utf8GetValidatedCharLength(str);
 }
 
 
-unsigned int EXPORT_THIS utf8UnicodeDecode(const char *str) {
+unsigned int EXPORT_THIS darnitUtf8UnicodeDecode(const char *str) {
 	return utf8GetChar(str);
 }
 
 
-int EXPORT_THIS utf8UnicodeEncode(unsigned int ch, char *str, int buf_len) {
+int EXPORT_THIS darnitUtf8UnicodeEncode(unsigned int ch, char *str, int buf_len) {
 	return utf8Encode(ch, str, buf_len);
 }
 
 
-int EXPORT_THIS utf8UnicodeEncodedLength(unsigned int ch) {
+int EXPORT_THIS darnitUtf8UnicodeEncodedLength(unsigned int ch) {
 	return utf8EncodedLength(ch);
 }
 
 
-int EXPORT_THIS utf8UnicodeCharactersInString(const char *str) {
+int EXPORT_THIS darnitUtf8UnicodeCharactersInString(const char *str) {
 	return utf8GetGlyphsInString(str);
 }
 
 
-int EXPORT_THIS utf8UnicodeCharactersInStringCounted(const char *str, int buf_len) {
+int EXPORT_THIS darnitUtf8UnicodeCharactersInStringCounted(const char *str, int buf_len) {
 	return utf8CountedStringSize(str, buf_len);
 }
 
 
-int EXPORT_THIS utf8UnicodeCharacterStart(const char *str, unsigned int unicode_pos) {
+int EXPORT_THIS darnitUtf8UnicodeCharacterStart(const char *str, unsigned int unicode_pos) {
 	return utf8FindCharIndex(str, unicode_pos);
 }
