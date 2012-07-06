3a92263168cdcdff853715f40a0cfd0ffa261af4
Steven Arnow <steven@slaeshjag.org>
2012-07-06 14:19:05 +0200

    Fixed function names in UTF-8 api

--- include/darnit/darnit_utf8.h
+++ include/darnit/darnit_utf8.h
@@ -1,13 +1,13 @@
 #ifndef __DARNIT_UTF8_H__
 #define	__DARNIT_UTF8_H__
 
-int utf8CharLenghtGet(const char *str);
-unsigned int utf8UnicodeDecode(const char *str);
-int utf8UnicodeEncode(unsigned int ch, char *str, int buf_len);
-int utf8UnicodeEncodedLength(unsigned int ch);
-int utf8UnicodeCharactersInString(const char *str);
-int utf8UnicodeCharactersInStringCounted(const char *str, int buf_len);
-int utf8UnicodeCharacterStart(const char *str, unsigned int unicode_pos);
+int darnitUtf8CharLenghtGet(const char *str);
+unsigned int darnitUtf8UnicodeDecode(const char *str);
+int darnitUtf8UnicodeEncode(unsigned int ch, char *str, int buf_len);
+int darnitUtf8UnicodeEncodedLength(unsigned int ch);
+int darnitUtf8UnicodeCharactersInString(const char *str);
+int darnitUtf8UnicodeCharactersInStringCounted(const char *str, int buf_len);
+int darnitUtf8UnicodeCharacterStart(const char *str, unsigned int unicode_pos);
 
 
 #endif
