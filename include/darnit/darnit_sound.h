281ff4fbe870f1e5a1dff9ecaedf5aee16bec36e
Steven Arnow <steven@slaeshjag.org>
2012-07-08 10:48:30 +0200

    Removed unused argument in sound playback API

--- include/darnit/darnit_sound.h
+++ include/darnit/darnit_sound.h
@@ -13,7 +13,7 @@ void *darnitSoundLoadStreamed(const char *fname, int preload, int makemono);
 void *darnitSoundUnload(void *sound_resource);
 void darnitSoundPlaybackStopAll(void *handle);
 void darnitSoundPlaybackStop(void *handle, int playback_key);
-int darnitSoundPlaybackStart(void *handle, void *sound_resource, int channels, int loop, int vol_l, int vol_r, int jmpto);
+int darnitSoundPlaybackStart(void *handle, void *sound_resource, int loop, int vol_l, int vol_r, int jmpto);
 int darnitSoundPlaybackCheck(void *handle, int playback_key);
 void darnitSoundPlaybackVolumeSet(void *handle, unsigned int playback_key, int vol_l, int vol_r);
 
