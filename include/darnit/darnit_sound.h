895bf9ef62b64ddec3bd086750995c2cac4b7781
Steven Arnow <steven@slaeshjag.org>
2012-07-08 08:06:11 +0200

    Added force-downmix, adjusted the mixing function in an attempt to reduce distorsion

--- include/darnit/darnit_sound.h
+++ include/darnit/darnit_sound.h
@@ -1,6 +1,13 @@
 #ifndef __DARNIT_AUDIO_H__
 #define	__DARNIT_AUDIO_H__
 
+#define			DARNIT_AUDIO_MONO		1
+#define			DARNIT_AUDIO_STEREO		2
+
+#define			DARNIT_AUDIO_PRELOAD		0
+#define			DARNIT_AUDIO_STREAM		1
+
+
 void *darnitSoundLoadTracked(const char *fname, int preload, int makemono);
 void *darnitSoundLoadStreamed(const char *fname, int preload, int makemono);
 void *darnitSoundUnload(void *sound_resource);
