0378ba084100d5b1ac80148cc59d99773232822b
Steven Arnow <s@rdw.se>
2012-12-28 21:12:37 +0100

    Forgot to include a struct in the API

--- include/darnit/darnit_stringtable.h
+++ include/darnit/darnit_stringtable.h
@@ -3,6 +3,12 @@
 
 typedef void DARNIT_STRINGTABLE;
 
+typedef struct DARNIT_STRINGTABLE_SECTIONS {
+	const char		**name;
+	unsigned int		names;
+} DARNIT_STRINGTABLE_SECTIONS;
+
+
 DARNIT_STRINGTABLE *darnitStringtableOpen(const char *fname);
 int darnitStringtableSectionLoad(DARNIT_STRINGTABLE *stringtable, const char *section);
 const DARNIT_STRINGTABLE_SECTIONS *darnitStringtableSectionList(DARNIT_STRINGTABLE *stringtable);
