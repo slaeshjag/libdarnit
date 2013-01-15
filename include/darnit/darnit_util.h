f628db47a3410fc81b63bba264fd7f5a6b486ced
Steven Arnow <s@rdw.se>
2013-01-15 10:22:41 +0100

    Exported internal fixed point sine function

--- include/darnit/darnit_util.h
+++ include/darnit/darnit_util.h
@@ -7,6 +7,7 @@ unsigned int d_util_ntohl(unsigned int arg);
 char *d_util_path_translate(const char *path);
 int d_util_string_to_int_array(const char *str, const char *delimiter, int *dest, int max_tokens);
 void d_util_endian_convert(unsigned int *block, int elements);
+int d_util_sin(int angle);
 
 
 #endif
