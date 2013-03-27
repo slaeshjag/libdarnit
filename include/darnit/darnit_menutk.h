b0e5d525830f2e37fbbbfaeaf7e6bddf9f6eab09
Steven Arnow <s@rdw.se>
2013-03-27 23:36:51 +0100

    Replaced void * with DARNIT_FONT in darnit_menutk.h

--- include/darnit/darnit_menutk.h
+++ include/darnit/darnit_menutk.h
@@ -28,11 +28,11 @@ freely, subject to the following restrictions:
 
 typedef void DARNIT_MENU;
 
-DARNIT_MENU *d_menu_horizontal_new(const char *options, int x, int y, void *font, int max_w);
-DARNIT_MENU *d_menu_vertical_new(const char *options, int x, int y, void *font, int menuw, int textskip_x, int max_h);
+DARNIT_MENU *d_menu_horizontal_new(const char *options, int x, int y, DARNIT_FONT *font, int max_w);
+DARNIT_MENU *d_menu_vertical_new(const char *options, int x, int y, DARNIT_FONT *font, int menuw, int textskip_x, int max_h);
 DARNIT_MENU *d_menu_vertical_shade_new(int x, int y, int shade_w, int shade_h, int y_advance, int options, int skip_option, int max_h);
-DARNIT_MENU *d_menu_spinbutton_new(const char *label, int x, int y, void *font, int step, int min, int max);
-DARNIT_MENU *d_menu_textinput_new(int x, int y, void *font, char *buff, int buff_len, int field_len);
+DARNIT_MENU *d_menu_spinbutton_new(const char *label, int x, int y, DARNIT_FONT *font, int step, int min, int max);
+DARNIT_MENU *d_menu_textinput_new(int x, int y, DARNIT_FONT *font, char *buff, int buff_len, int field_len);
 void d_menu_vertical_shade_selections_reduce(DARNIT_MENU *menu);
 void d_menu_vertical_shade_selections_increase(DARNIT_MENU *menu);
 void d_menu_shade_color(DARNIT_MENU *menu, unsigned char r, unsigned char g, unsigned char b, unsigned char a);
