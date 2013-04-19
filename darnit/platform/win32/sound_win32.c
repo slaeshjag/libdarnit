e042cad0d27739dc0c7b896e95a1c13cd925eb94
h4xxel <isaksson.axel@telia.com>
2013-04-19 14:26:17 +0200

    maybe working sound in winblows.. probably not though >_<

--- darnit/platform/win32/sound_win32.c
+++ darnit/platform/win32/sound_win32.c
@@ -29,29 +29,53 @@ freely, subject to the following restrictions:
 #include "../main.h"
 
 static const unsigned int[] sample_format = {
-	[TPW_SAMPLE_FORMAT_S16LE] = 16;
-	[TPW_SAMPLE_FORMAT_S8] = 8;
+	[TPW_SAMPLE_FORMAT_S16LE] = 16,
+	[TPW_SAMPLE_FORMAT_S8] = 8,
 };
 
-static WAVEFORMATEX waveformat;
-static HWAVEOUT handle;
+static struct CALLBACK_DATA {
+	void (*fetch_audio)(void *data, void *mixdata, int bytes);
+	void *userdata;
+} callback_data;
+
+static struct {
+	HWAVEOUT handle;
+	sound.formatEX format;
+	WAVEHDR header[2];
+	unsigned int current;
+} sound;
 
 static void callback(HWAVEOUT hwo, UINT msg, DWORD_PTR instance, DWORD_PTR param1, DWORD_PTR param2) {
+	if(msg!=WOM_DONE)
+		return;
+	callback_data.fetch_audio(callback_data.userdata, sound.header[sound.current].lpData, sound.header[sound.current].dwBufferLength);
+	waveOutWrite(handle, &(sound.header[sound.current]), sizeof(WAVEHDR));
 	
+	sound.current=!sound.current;
 }
 
 int tpw_sound_open(TPW_SOUND_SETTINGS settings) {
 	int res;
+	sound.format.wFormatTag = WAVE_FORMAT_PCM;
+	sound.format.nChannels = settings.channels;
+	sound.format.nSamplesPerSec = settings.sample_rate;
+	sound.format.nAvgBytesPerSec = settings.sample_rate*(sample_format[settings.format]/8)*settings.channels;
+	sound.format.nBlockAlign = (sample_format[settings.format]/8)*settings.channels;
+	sound.format.wBitsPerSample = sample_format[settings.format];
+	sound.format.cbSize = 0;
 	
-	waveformat.wFormatTag = WAVE_FORMAT_PCM;
-	waveformat.nChannels = settings.channels;
-	waveformat.nSamplesPerSec = settings.sample_rate;
-	waveformat.nAvgBytesPerSec = settings.sample_rate*(sample_format[settings.format]/8)*setting.channels;
-	waveformat.nBlockAlign = (sample_format[settings.format]/8)*setting.channels;
-	waveformat.wBitsPerSample = sample_format[settings.format];
-	waveformat.cbSize = 0;
-	if(waveOutOpen(&handle, WAVE_MAPPER, &waveformat, (DWORD_PTR) callback, NULL, CALLBACK_FUNCTION)==MMSYSERR_NOERROR) {
-		waveOutPause(handle);
+	callback_data.fetch_audio=settings.callback;
+	callback_data.userdata=settings.userdata;
+	callback_data.samples=settings.samples;
+	
+	sound.header[0].dwBufferLength=sound.header[1].dwBufferLength=(sample_format[settings.format]/8)*settings.samples
+	sound.header[0].lpData=malloc(sound.header[0].dwBufferLength);
+	sound.header[1].lpData=malloc(sound.header[1].dwBufferLength);
+	
+	if(waveOutOpen(&sound.handle, WAVE_MAPPER, &sound.format, (DWORD_PTR) callback, 0, CALLBACK_FUNCTION)==MMSYSERR_NOERROR) {
+		waveOutPrepareHeader(sound.handle, &(sound.handle[0]), sizeof(WAVEHDR));
+		waveOutPrepareHeader(sound.handle, &(sound.handle[1]), sizeof(WAVEHDR));
+		waveOutPause(sound.handle);
 		return 0;
 	}
 	return -1;
@@ -59,8 +83,13 @@ int tpw_sound_open(TPW_SOUND_SETTINGS settings) {
 
 
 void tpw_sound_control(TPW_SOUND_COMMAND command) {
-	if(command==TPW_SOUND_COMMAND_PLAY)
-		waveOutRestart(handle);
-	else if(command==TPW_SOUND_COMMAND_PAUSE)
-		waveOutPause(handle);
+	switch(command) {
+		case TPW_SOUND_COMMAND_PLAY:
+			waveOutRestart(sound.handle);
+			callback(sound.handle, WOM_DONE, 0, 0, 0);
+			break;
+		case TPW_SOUND_COMMAND_PAUSE:
+			waveOutPause(sound.handle);
+			break;
+	}
 }
