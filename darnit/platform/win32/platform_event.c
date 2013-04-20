8209e3bfbb20fd4ecb9b472f016eb5450a51031d
h4xxel <isaksson.axel@telia.com>
2013-04-20 16:02:44 +0200

    fixed another build error

--- darnit/platform/win32/platform_event.c
+++ darnit/platform/win32/platform_event.c
@@ -125,6 +125,7 @@ LRESULT CALLBACK tpw_message_process(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM
 
 void tpw_event_loop() {
 	#warning tpw_event_loop(): Probably incomplete implementation
+	MSG msg;
 	while PeekMessage(&msg, NULL, 0, 0, PM_REMOVE) {
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
