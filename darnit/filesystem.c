cfa9f613efd93ca363e06966c8532b6f47488cdb
Steven Arnow <s@rdw.se>
2013-05-04 23:15:21 +0200

    Some debug output..

--- darnit/filesystem.c
+++ darnit/filesystem.c
@@ -815,6 +815,7 @@ FILESYSTEM_FILE *fsGetRealFile(const char *path_src) {
 		}
 		#endif
 		/* Here, the new file needs to be created */
+		fprintf(stderr, "Opening new file %s\n", new_file);
 		if (!(dest = fsFileOpen(new_file, "w"))) {
 			free(new_file);
 			return NULL;
