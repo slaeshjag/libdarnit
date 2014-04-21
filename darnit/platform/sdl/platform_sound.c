89be91c3a819eaba464b50f4236a76e75fe52e0e
Steven Arnow <s@rdw.se>
2014-04-21 22:56:17 +0200

    Callback arguments are wrong, but I'll fix that tomorrow

--- darnit/platform/sdl/platform_sound.c
+++ darnit/platform/sdl/platform_sound.c
@@ -80,18 +80,21 @@ void tpw_sound_control(TPW_SOUND_COMMAND command) {
 #include <alsa/asoundlib.h>
 
 struct {
-	void			(*callback)(void *sample, int samples, void *user_data);
+	void			(*callback)(int channels, void *sample, int samples, void *user_data);
 	void			*user_data;
 	snd_pcm_t		*alsa_handle;
+	int			channels;
 } tpw_mic_capture;
 
 
-void tpw_sound_capture_handler() {
+void tpw_sound_capture_handler(void *sample, int samples) {
+	if (tpw_mic_capture.callback)
+		tpw_mic_capture.callback(tpw_mic_capture.channels, sample, samples, tpw_mic_capture.user_data);
 	return;
 }
 
 
-void twp_sound_capture_init() {
+void tpw_sound_capture_init() {
 	tpw_mic_capture.alsa_handle = NULL;
 	tpw_mic_capture.user_data = NULL;
 	tpw_mic_capture.callback = NULL;
@@ -99,18 +102,97 @@ void twp_sound_capture_init() {
 	return;
 }
 
-
-int tpw_sound_capture_register(void (*callback)(void *, int, void *), void *user_data) {
-	snd_pcm_hw_params_t *hw_params;
+int tpw_sound_capture_attempt_init(const char *device, int channels) {
 	int err;
+	unsigned int rate;
+	snd_pcm_hw_params_t *hw_params;
+	snd_async_handler_t *pcm_callback;
+	char *err_str;
+	rate = 44100;
+
+	hw_params = NULL;
+
+	if ((err = snd_pcm_open(&tpw_mic_capture.alsa_handle, device, SND_PCM_STREAM_CAPTURE, SND_PCM_NONBLOCK)) < 0) {
+		err_str = "Unable to open audio device";
+		goto error;
+	}
+
+	if ((err = snd_pcm_hw_params_malloc(&hw_params)) < 0) {
+		err_str = "Unable to alloc audio hardware parameter structure";
+		goto error;
+	}
+
+	if ((err = snd_pcm_hw_params_any(tpw_mic_capture.alsa_handle, hw_params)) < 0) {
+		err_str = "Unable to get default hardware parameters";
+		goto error;
+	}
+
+	if ((err = snd_pcm_hw_params_set_access(tpw_mic_capture.alsa_handle, hw_params, SND_PCM_ACCESS_RW_INTERLEAVED)) < 0) {
+		err_str = "Unable to set capture access type";
+		goto error;
+	}
+
+	if ((err = snd_pcm_hw_params_set_format(tpw_mic_capture.alsa_handle, hw_params, SND_PCM_FORMAT_S16_LE)) < 0) {
+		err_str = "Unable to set capture sample format";
+		goto error;
+	}
+
+	if ((err = snd_pcm_hw_params_set_rate_near(tpw_mic_capture.alsa_handle, hw_params, &rate, 0)) < 0) {
+		err_str = "Unable to set capture sample rate";
+		goto error;
+	}
+
+	if ((err = snd_pcm_hw_params_set_channels(tpw_mic_capture.alsa_handle, hw_params, channels)) < 0) {
+		err_str = "Unable to set capture sample channels";
+		goto error;
+	}
+
+	if ((err = snd_pcm_hw_params(tpw_mic_capture.alsa_handle, hw_params)) < 0) {
+		err_str = "Unable to apply capture hardware parameters";
+		goto error;
+	}
+
+	snd_pcm_hw_params_free(hw_params);
+	hw_params = NULL;
+
+	if ((err = snd_pcm_prepare(tpw_mic_capture.alsa_handle)) < 0) {
+		err_str = "Unable to prepare capture hardware for use";
+		goto error;
+	}
+	
+	snd_async_add_pcm_handler(&pcm_callback, tpw_mic_capture.alsa_handle, tpw_sound_capture_handler, NULL);
+	snd_pcm_start(tpw_mic_capture.alsa_handle);
+	
+	return 1;
+	
+	error:
+		fprintf(stderr, "Capture hardware failed to open: %s: %s\n", err_str, snd_strerror(err));
+		if (hw_params)
+			free(hw_params);
+		if (tpw_mic_capture.alsa_handle)
+			snd_pcm_close(tpw_mic_capture.alsa_handle);
+		tpw_mic_capture.alsa_handle = NULL;
+			
+	return 0;
+}
+
+
+int tpw_sound_capture_register(void (*callback)(int, void *, int, void *), void *user_data) {
 
 	tpw_mic_capture.callback = callback;
 	tpw_mic_capture.user_data = user_data;
 
 	if (tpw_mic_capture.alsa_handle)
 		return 1;
+	if (tpw_sound_capture_attempt_init("default:0", 2))
+		return 1;
+	if (tpw_sound_capture_attempt_init("default:0", 1))
+		return 1;
+	if (tpw_sound_capture_attempt_init("hw:0,0", 2))
+		return 1;
+	if (tpw_sound_capture_attempt_init("hw:0,0", 1))
+		return 1;
 	
-//	if ((err = snd_pcm_open(&tpw_mic_capture.alsa_handle, "default:0,0", SND_STREAM_CAPTURE, SND_
 
 	return 0;
 }
